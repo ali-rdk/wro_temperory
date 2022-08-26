2943980e8519a04f85f2639bdbf17ae9126f42fe
parshan-80 <pparshanmoini@gmail.com>
2022-08-26 12:28:20 +0430

    Update main2.ino

--- main2/main2.ino
+++ main2/main2.ino
@@ -9,18 +9,21 @@ UltraSonicDistanceSensor ultra_c(3, 2);
 UltraSonicDistanceSensor ultra_l(6, 7);  
 Servo servo_1;
 int servo = 9;
-float error;
-float correction;
-float straught_distance;
-float right_distance;
 int motor1 = 10;
 int motor2 = 11;
+
+int middle_servo = 90;
+
+float wall_c = 0;
 int state = 0;
 int side = 0;
 long timer = 0;
-int servo_position;
 int target_angle = 0;
-int middle_servo = 90;
+float error;
+float correction;
+int servo_position;
+float straught_distance,c,r,l;
+float right_distance;
 
 void setup() {
     pinMode(motor1, OUTPUT);
@@ -55,18 +58,27 @@ void loop() {
 
   error = mpu.getAngleZ()- target_angle;
   correction = middle_servo + (error*1);
+  correction +=  wall_c;
   if (correction >= 100){correction = 100;}
   if (correction <= 20){correction = 20;}
-  servo_1.write(correction);
+  servo_1.write(correction );
   straught_distance = 0;
+  
   for (int i=0; i != 3; i++){straught_distance = straught_distance + ultra_c.measureDistanceCm();}
   straught_distance = straught_distance/3;
-  if (straught_distance <= 70 && straught_distance >= 5 && ultra_c.measureDistanceCm()<=70&&ultra_c.measureDistanceCm()>=5 && millis()-timer >=3000 &&abs(mpu.getAngleZ()- target_angle)<=5  ){
+
+  c = ultra_c.measureDistanceCm();
+  r = ultra_r.measureDistanceCm();
+  l = ultra_l.measureDistanceCm();
+  if (r <= 10 && r >= 5){wall_c = -130/r;}
+  else if (l <= 10 && l >= 5){wall_c = 130/l;}
+  else{wall_c = 0;}
+  if (straught_distance <= 70 && straught_distance >= 5 && c<=70 && c>=5 && millis()-timer >=2000 &&abs(mpu.getAngleZ()- target_angle)<=20  ){
     if (state == 0){
       right_distance = 0;
-      for (int i=0; i != 3; i++){right_distance = right_distance + ultra_r.measureDistanceCm();}
+      for (int i=0; i != 3; i++){right_distance = right_distance + r;}
         right_distance = right_distance/3;
-      if (right_distance <= 120 && right_distance >= 5 && ultra_r.measureDistanceCm() <= 120 && ultra_r.measureDistanceCm()>=5){side = -1;}
+      if (right_distance <= 120 && right_distance >= 5 && r <= 120 && r>=5){side = -1;}
       else{side = -1;}
       state = 1;
     }
