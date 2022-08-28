78b88d6026d32a98a28952031197670b3436b3d0
ali roodaki <44902373+ali-rdk@users.noreply.github.com>
2022-08-28 17:58:54 +0100

    Add files via upload

--- color detection.py
+++ color detection.py
@@ -1,36 +1,32 @@
 import numpy as np
 import cv2
-import RPi.GPIO as GPIO
 import serial
 import time
 
-# x coordinate
-# y coordinate
-# w : width
-# h : height
-# z : erea of rectangle
-# 630
-r_cw = 0
 
-arduino =  serial.Serial("/dev/ttyACM0", 9600, timeout=1)
-time.sleep(1)
-if arduino.isOpen():
-    print("conected")
-g_area = 0
-g_cw = -1
-r_area = 0
-r_cw = -1
-#pos.reset_input_buffer()
+#arduino =  serial.Serial("/dev/ttyACM0", 9600, timeout=1)
+#time.sleep(1)
+#if arduino.isOpen():
+#    print("conected")
+    
+g_find = False
+r_find = False
+red_area = 0
+green_area = 0
+
 
 webcam = cv2.VideoCapture(0)
 while(1):
-    cmd = ""
     _, imageFrame = webcam.read()
+    
+    imageFrame = cv2.blur(imageFrame, (10,10))
+    imageFrame = cv2.resize(imageFrame,(480,300))
+    
     imageFrame = cv2.flip(imageFrame,0)
   
     hsvFrame = cv2.cvtColor(imageFrame, cv2.COLOR_BGR2HSV)
   
-    red_lower = np.array([136, 87, 111], np.uint8)
+    red_lower = np.array([150, 100, 111], np.uint8)
     red_upper = np.array([180, 255, 255], np.uint8)
     red_mask = cv2.inRange(hsvFrame, red_lower, red_upper)
     
@@ -38,7 +34,7 @@ while(1):
     green_upper = np.array([102, 255, 255], np.uint8)
     green_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
 
-    kernal = np.ones((5, 5), "uint8")
+    kernal = np.ones((20,20), "uint8")
       
     # For red color
     red_mask = cv2.dilate(red_mask, kernal)
@@ -50,114 +46,64 @@ while(1):
     # Creating contour to track red color
     r_contours, r_hierarchy = cv2.findContours(red_mask,cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     g_contours, g_hierarchy = cv2.findContours(green_mask,cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    r_area = 0;
-    r_cw = -1
+    
+    r_flag = False
     for pic, contour in enumerate(r_contours):
         r_areas = [cv2.contourArea(c) for c in r_contours]
         max_index = np.argmax(r_areas)
         cnt=r_contours[max_index]
         M = cv2.moments(contour)
         r_area = cv2.contourArea(contour)
-        if(r_area > 300):
+        
+        if(r_area > 1000):
             x, y, w, h = cv2.boundingRect(cnt)
             imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        
-              
-            cv2.putText(imageFrame, "Red Colour", (x, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1.0,
-                        (0, 0, 255))    
-            #cx = int(M['m10']/M['m00'])
-            #cy = int(M['m01']/M['m00'])
             r_cw = int(M['m10']/M['m00'])
-            r_ch = int(M['m01']/M['m00'])
-            #r_area = h
-        #else:
-         #   r_area = 0
+            red_area = h * w
+            r_flag = True
         
             
 
-    
+    g_flag = False
     for pic, contour in enumerate(g_contours):
         g_areas = [cv2.contourArea(c) for c in g_contours]
         max_index = np.argmax(g_areas)
         cnt=g_contours[max_index]
         M = cv2.moments(contour)
         g_area = cv2.contourArea(contour)
-        if(g_area > 300):
+        green_area = 0 
+        if(g_area > 1000):
             x, y, w, h = cv2.boundingRect(cnt)
             imageFrame = cv2.rectangle(imageFrame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        
-              
-            cv2.putText(imageFrame, "Green Colour", (x, y),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1.0,
-                        (0, 255, 0))    
-            #cx = int(M['m10']/M['m00'])
-            #cy = int(M['m01']/M['m00'])
             g_cw = int(M['m10']/M['m00'])
-            g_ch = int(M['m01']/M['m00'])
-        else:
-            g_area = 0
+            green_area = h * w
+            g_flag = True
 
 
 
     # Program Termination
-    cmd = 0
-    if g_area <= r_area and r_area != 0 :
-        if r_cw <= 100:
-            cmd = 0
-        elif r_cw <= 150:
-            cmd = 1
-        elif r_cw <= 250:
-            cmd = 2
-        elif r_cw <= 300:
-            cmd = 3
-        elif r_cw <= 300:
-            cmd = 4
-        elif r_cw <= 350:
-            cmd = 5
-        elif r_cw <= 400:
-            cmd = 6
-        elif r_cw <= 500:
-            cmd = 7
-        elif r_cw <= 600:
-            cmd = 8
-        elif r_cw >= 600:
-            cmd = 9
-            r_area = 20000
-        cmd = cmd * (r_area/10000)
+    if not r_flag and not g_flag:
+        cmd = 0
+   
+    elif r_flag and green_area <= red_area :
+        cmd = (r_cw / 50) - 2     
+        cmd = round(cmd * (red_area/5000)* -1)
         
-    if g_area >= r_area and g_area != 0 :
-        g_cw = abs(650 - g_cw)
-        if g_cw >= 0 and g_cw < 150:
-            cmd = 0
-        elif g_cw <= 150:
-            cmd = 1
-        elif g_cw <= 250:
-            cmd = 2
-        elif g_cw <= 300:
-            cmd = 3
-        elif g_cw <= 350:
-            cmd = 4
-        elif g_cw <= 400:
-            cmd = 5
-        elif g_cw <= 450:
-            cmd = 6
-        elif g_cw <= 500:
-            cmd = 7
-        elif g_cw <= 600:
-            cmd = 8
-        elif g_cw >= 600:
-            cmd = 9
-            g_area = 20000
-            
-        cmd *= -1
-        cmd = cmd * (g_area/10000)
+    elif g_flag and green_area >= red_area :
+        g_cw = abs(480 - g_cw)
+        cmd = (g_cw / 50) - 2      
+        cmd = round(-cmd * (green_area/5000) )
+    else:
+        cmd = 0
         
-    #print(cmd)
-    cmd = round(cmd)
-    arduino.write(str(cmd).encode())
+        
+    #arduino.write(str(cmd*5).encode())
     print(cmd)
     cv2.imshow("Multiple Color Detection in Real-TIme", imageFrame)
+
+
+
+
     if cv2.waitKey(10) & 0xFF == ord('q'):
         webcam.release()
         cv2.destroyAllWindows()
